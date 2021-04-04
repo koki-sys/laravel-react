281ff9cbb182e656a9e0e8ce657dadfa2e2a1779
koki-sys <soraisu7@gmail.com>
2021-04-04 23:38:53 +0900

    deploy herokuQ

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function getTodos(){
+        $todos = Todo::all();
+        return $todos;
+    }
+
+    public function addTodo(Request $request){
+        $todo = new Todo;
+        $todo->title = $request->title;
+        $todo->save();
+
+        $todos = Todo::all();
+        return $todos;
+    }
+
+    public function deleteTodo(Request $request) {
+        $todo = Todo::find($request->id);
+        $todo->delete();
+
+        $todos = Todo::all();
+        return $todos;
+    }
+}
---
+++ resources/js/components/TodoApp.js
@@ -0,0 +1,119 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+function RenderRows(props) {
+    return props.todos.map(todo => {
+        return (
+            <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+                <td><button className="btn btn-secondary" onClick={() => props.deleteTask(todo)}>完了</button></td>
+            </tr>
+        );
+    });
+}
+
+export default class TodoApp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [],
+            todo: ''
+        };
+        this.inputChange = this.inputChange.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
+    }
+
+    componentDidMount() {
+        axios
+            .get('./api/get')
+            .then((res) => {
+                this.setState({
+                    todos: res.data
+                });
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    inputChange(event) {
+        switch (event.target.name) {
+            case 'todo':
+                this.setState({
+                    todo: event.target.value
+                });
+                break;
+            default:
+                break;
+        }
+    }
+
+    addTodo() {
+        if (this.state.todo == '') {
+            return;
+        }
+
+        axios
+            .post('/api/add', {
+                title: this.state.todo
+            })
+            .then((res) => {
+                this.setState({
+                    todos: res.data,
+                    todo: ''
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    deleteTask(todo) {
+        axios.post('/api/del', {
+            id: todo.id
+        })
+        .then((res) => {
+            this.setState({
+                todos: res.data
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="form-group mt-4">
+                    <label htmlFor="todo">新規Todo</label>
+                    <input type="text" className="form-control" name='todo' value={this.state.todo} onChange={this.inputChange} />
+                </div>
+                <button className="btn btn-primary" onClick={this.addTodo}>登録</button>
+                <table className="table mt-5">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>タスク</th>
+                            <th>完了</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/*行の描画*/}
+                        <RenderRows
+                            todos={this.state.todos}
+                            deleteTask={this.deleteTask}
+                        />
+
+                    </tbody>
+                </table>
+            </React.Fragment>
+        )
+    }
+
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('todoApp'));
